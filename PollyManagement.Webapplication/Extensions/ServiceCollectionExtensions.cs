106a71d145feb4d57b33514cc9d970b826b94571
Koen Ekelschot <koen.ekelschot@gmail.com>
2020-06-11 21:15:38 +0200

    Use ServiceCollection extension

---
+++ PollyManagement.Webapplication/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Polly.CircuitBreaker;
+using PollyManagement.PolicyManager;
+using System.Collections.Generic;
+
+namespace PollyManagement.Webapplication.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddPollyPolicy<TPolicy>(this IServiceCollection services, string policyName, TPolicy policy) where TPolicy : ICircuitBreakerPolicy
+        {
+            var manager = GetOrAddManager(services);
+            manager.TryAdd(policyName, policy);
+
+            return services;
+        }
+
+        public static IServiceCollection AddPollyPolicies<TPolicy>(this IServiceCollection services, IDictionary<string, TPolicy> policies) where TPolicy : ICircuitBreakerPolicy
+        {
+            var manager = GetOrAddManager(services);
+            foreach (var policy in policies)
+            {
+                manager.TryAdd(policy.Key, policy.Value);
+            }
+
+            return services;
+        }
+
+        private static CircuitBreakerManager GetOrAddManager(IServiceCollection services)
+        {
+            CircuitBreakerManager manager;
+
+            using (var provider = services.BuildServiceProvider())
+            {
+                manager = provider.GetService<CircuitBreakerManager>();
+                if (manager == null)
+                {
+                    manager = new CircuitBreakerManager();
+                    services.AddSingleton(manager);
+                }
+            }
+
+            return manager;
+        }
+    }
+}
--- PollyManagement.Webapplication/Startup.cs
+++ PollyManagement.Webapplication/Startup.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Polly;
 using Polly.CircuitBreaker;
-using PollyManagement.PolicyManager;
+using PollyManagement.Webapplication.Extensions;
 
 namespace PollyManagement
 {
@@ -26,9 +26,7 @@ namespace PollyManagement
                 .AddControllersAsServices()
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-            var circuitBreakerManager = new CircuitBreakerManager();
-            circuitBreakerManager.TryAdd("Ideal-Policy", policy);
-            services.AddSingleton(circuitBreakerManager);
+            services.AddPollyPolicy("Ideal-Policy", policy);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
